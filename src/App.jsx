fb167cfcae79586b78e6d9e961e0e76f3b92e7a3
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-12-06 14:57:33 +0100

    chore: clean boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div>
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="w-24 rounded-sm bg-purple-900 py-1 text-purple-50"
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1 className="text-2xl">Hello React, Vite, and Tailwind!!</h1>
+    </main>
   );
 }
 
