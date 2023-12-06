5cfbd96fdcd43ead26e4396febcabbafe64c7bb6
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-12-06 17:55:11 +0100

    feat: add interpolation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { IntlProvider } from "react-intl";
+import Interpolation from "./components/Interpolation";
+
 function App() {
   return (
-    <main>
-      <h1 className="text-2xl">Hello React, Vite, and Tailwind!!</h1>
-    </main>
+    <IntlProvider locale="en-US" defaultLocale="en-US">
+      <main className="mx-auto max-w-[800px] rounded-md border-t-2 border-indigo-500 bg-black/20 px-6 pt-6">
+        <h1 className="text-4xl font-bold">ICU Demo</h1>
+        <p className="mt-4 text-xl font-thin">
+          This is an interactive demo that showcases the capabilities of ICU
+          (International Components for Unicode).
+        </p>
+
+        <hr className="mt-4 h-[0.5px] border-0 bg-indigo-400" />
+
+        <h2 className="pt-4 text-2xl font-semibold">Interpolation</h2>
+        <p className="mt-4">
+          Arguably the most important component of ICU is the message format. It
+          allows you to create messages that can be translated into different
+          languages. You can also interpolate runtime variables into your
+          messages.
+        </p>
+
+        <Interpolation />
+      </main>
+    </IntlProvider>
   );
 }
 
---
+++ src/components/Interpolation.jsx
@@ -0,0 +1,83 @@
+import { parse } from "@formatjs/icu-messageformat-parser";
+import { useState } from "react";
+import { FormattedMessage } from "react-intl";
+
+function variablesFor(message) {
+  try {
+    const ast = parse(message);
+    const variables = ast
+      .map((node) => [1, 6].includes(node.type) && node.value)
+      .filter(Boolean);
+    return [variables, null];
+  } catch (e) {
+    console.error(e);
+    return [[], e.message];
+  }
+}
+
+export default function Interpolation() {
+  const [message, setMessage] = useState("Hello, {name}!");
+  const [variables, parsingError] = variablesFor(message);
+  const [values, setValues] = useState(
+    variables.reduce((acc, cur) => {
+      acc[cur] = `{${cur}}`;
+      return acc;
+    }, {}),
+  );
+
+  return (
+    <div className="mt-2 flex justify-between gap-12">
+      <div className="flex-1">
+        <label
+          htmlFor="interpolatedMessage"
+          className="mt-2 block text-xl font-medium"
+        >
+          Message
+        </label>
+        <textarea
+          id="interpolatedMessage"
+          className="mt-2 w-full rounded-sm bg-indigo-50 p-2 text-indigo-950"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+
+        <h4 className="mt-3 block text-lg">Variables</h4>
+        {/* Map variables to labeled inputs */}
+        {variables.map((variable) => (
+          <div key={variable} className="mt-2 flex items-center gap-2">
+            <label htmlFor={variable} className="basis-28 font-semibold">
+              {variable}
+            </label>
+            <input
+              id={variable}
+              className="flex-grow rounded-sm bg-indigo-50 p-2 text-indigo-950"
+              placeholder={variable}
+              value={values[variable] || ""}
+              onChange={(e) =>
+                setValues((prev) => ({ ...prev, [variable]: e.target.value }))
+              }
+            />
+          </div>
+        ))}
+      </div>
+
+      <div className="flex-1">
+        <h3 className="mt-2 block text-xl font-medium">Output</h3>
+
+        <p className="mt-2 min-h-[80px] rounded-sm bg-black/50 p-4">
+          {parsingError ? (
+            <span className="text-red-400">
+              Invalid message: {parsingError}
+            </span>
+          ) : (
+            <FormattedMessage
+              id={message || "..."}
+              defaultMessage={message || "..."}
+              values={values}
+            />
+          )}
+        </p>
+      </div>
+    </div>
+  );
+}
